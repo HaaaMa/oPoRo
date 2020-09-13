30cd2b5001471d9399cbd5393bb576efc7f54941
Glitch (alpine-ionized-handstand) <none>
2020-09-13 21:12:17 +0000

    📟⚾️ Checkpoint ./commands/moderation/tempmute.js:13134877/797 ./package.json:13134877/1634

--- commands/moderation/tempmute.js
+++ commands/moderation/tempmute.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+const ms = require("ms");
 const { MessageEmbed } = require("discord.js");
 const { Color } = require("../../config.js");
 
@@ -31,22 +32,36 @@ module.exports = {
     
     let Time = args.slice(1).join(" ");
     
-    
+    if (!Time) return message.channel.send(`Please Give Time Example : 1d!`);
 
     let Embed = new MessageEmbed()
       .setColor(Color)
       .setTitle(`Member Muted!`)
       .addField(`Moderator`, `${message.author.tag} (${message.author.id}`)
       .addField(`Muted Member`, `${Member.user.tag} (${Member.user.id})`)
+      .addField(`Muted For`, `${Time}`)
+      .setFooter(`Requested by ${message.author.username}`)
+      .setTimestamp();
+    
+    let Embed2 = new MessageEmbed()
+      .setColor(Color)
+      .setTitle(`Member Unmuted!`)
+      .addField(`Moderator`, `${message.author.tag} (${message.author.id}`)
+      .addField(`Unmuted Member`, `${Member.user.tag} (${Member.user.id})`)
       .setFooter(`Requested by ${message.author.username}`)
       .setTimestamp();
 
     if (Role && !Member.user.roles.cache.has(Role)) {
       Member.user.roles.add([Role]);
-      return message.channel.send(Embed);
+      message.channel.send(Embed);
     } else {
-      return message.channel.send(`Something Went Wrong, Try Again Later!`);
+      message.channel.send(`Something Went Wrong, Try Again Later!`);
     };
+    
+    setTimeout(function() {
+      Member.user.roles.remove([Role]);
+      message.channel.send(Embed2);
+    }, ms(Time))
 
     //End
   }
