457cc3a31d3812ed64f96f9c5b188c974f1a12a1
Glitch (alpine-ionized-handstand) <none>
2020-09-15 16:51:17 +0000

    🔔👡 Checkpoint ./config.js:22347487/183

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-exports.Prefix = `PUT YOUR BOT PREFIX HERE`;
-exports.Token = `PUT YOUR TOKEN HERE`;
-exports.Color = `PUT COLOR OF EMBED HERE`;
+exports.Prefix = `!`;//your prefix for bot
+exports.Token = `NzU1NDY4Mzc4NTQ5OTExNjYz.X2DuwQ.iCkpOtIreJWD8ugKCR0U3Mdy0Eg`;//your token 
+exports.Color = `RED`;//color   of embed
