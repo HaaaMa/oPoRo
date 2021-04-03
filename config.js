891f278fa1e56a8ce50d0097292b5281645d59d0
Glitch (alpine-ionized-handstand) <none>
2021-04-03 01:48:49 +0000

    🖖⛄️ Checkpoint ./commands/moderation/prefix.js:30514896/1648 ./server.js:30514896/1793 ./config.js:30514896/206

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-exports.Prefix = `>`;//your prefix for bot
-exports.Token = `ODEzMTM3NDYwNjczNjQyNTI3.YDK7RQ.DzHvTvI3bKMbP4sDxZYLUsquLQo`;//your token 
-exports.Color = `RANDOM`;//color   of embed
+exports.Prefix = `>`;//command Bo botaka
+exports.Token = ``; /// token Bot 
+exports.Color = `RANDOM`;// Rangek Bo embed belp
