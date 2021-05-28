c3e451a8f7c785dcbac2053cb421be62053263a3
Glitch (alpine-ionized-handstand) <none>
2021-05-28 17:01:09 +0000

    ⏰🦀 Checkpoint ./config.js:31825723/115

--- config.js
+++ config.js
@@ -1,3 +1,14 @@
 exports.Prefix = `>`;//command Bo botaka
 exports.Token = `ODQ2MzQyMjY4NTU4NjM5MTI1.YKuHqQ.Ua4R7V1q4jTPv3fEz4SmFycqKhQ`; /// token Bot 
 exports.Color = `RANDOM`;// Rangek Bo embed belp
+
+
+
+
+/////////        H      A     a    A    A M  A 
+
+
+
+
+
+/// >>> HaaaMa create 
