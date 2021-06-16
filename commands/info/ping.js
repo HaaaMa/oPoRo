f856655fc2bd9c9954f5f3bc6eea01c41482d52e
Black <82063930+Blackkk0@users.noreply.github.com>
2021-06-16 18:09:28 +0300

    Update ping.js

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -8,17 +8,14 @@ module.exports = {
   description: "Pong!",
   usage: "Ping",
   run: async (client, message, args) => {
-    //Start
-    message.delete();
+    
 
     const embed = new MessageEmbed()
       .setColor(Color)
       .setDescription(`Pong - ${client.ws.ping}`)
-      .setFooter(`Requested By ${message.author.username}`)
-      .setTimestamp();
 
     message.channel.send(embed);
 
-    //End
+    
   }
 };
