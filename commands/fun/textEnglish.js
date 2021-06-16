c0e6eee9b8e16cafe33eac659bbc6376fa2217c4
Black <82063930+Blackkk0@users.noreply.github.com>
2021-06-16 17:39:17 +0300

    Update and rename text3.js to textEnglish.js

---
+++ commands/fun/textEnglish.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+const { MessageEmbed } = require("discord.js");
+const { Color } = require("../../config.js");
+
+module.exports = {
+  name: "textEnglish",
+  aliases: [],
+  description: "Show text",
+  usage: "text",
+  run: async (client, message, args) => {
+    
+    message.delete();
+    let sizes = ["","","","","","","","","","","","","","",""];
+
+    let Member =
+      message.mentions.members.first() ||
+      message.guild.members.cache.get(args[0]) ||
+      message.member;
+
+    let Result = sizes[Math.floor(Math.random() * sizes.length)];
+
+    let embed = new MessageEmbed()
+      .setColor(Color)
+      .setDescription(`\n${Result}`)
+
+    message.channel.send(embed);
+
+    
+  }
+};
