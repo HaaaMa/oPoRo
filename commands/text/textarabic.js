e909d445ea29bdec8d701c853ac7ae0b56f3a5fb
Black <82063930+Blackkk0@users.noreply.github.com>
2021-06-16 19:43:30 +0300

    Update textarabic.js

--- commands/text/textarabic.js
+++ commands/text/textarabic.js
@@ -4,13 +4,13 @@ const { Prefix } = require("../../config.js");
 
 module.exports = {
   name: "textarabic",
-  aliases: [],
+  aliases: ["textArabic"],
   description: "Show text",
   usage: "text",
   run: async (client, message, args) => {
     
     
-    let sizes = ["","","","","","","","","","","","","","",""];
+    let sizes = ["إذا زارتک شدّه فاعلم أنّها سحابه صیف عن قلیل تقشع، ولا یخیفک رعدها، ولا یرهبک برقها، فربّما کانت محمّلهً بالغیث","چنت اعاتب بالوكت عرفني بيك :broken_heart: :exclamation:","أخاف أفگد طعم باچر يالمتانيك:wilted_rose:","اسألني واني اختصر لعيونك المده :wilted_rose:","وأستحيت من الله حيل ، وآنه سآجد مر ذكرهم والتهيت :wilted_rose:","ما خربتني وياك غير اليكولون:wilted_rose:","شگلّك يالعلي تسأل.. انا التيهت كلشي وكلشي ما اندل:wilted_rose:","هي مسؤولية من تحبلك انسان :broken_heart: "];
 
     let Member =
       message.mentions.members.first() ||
