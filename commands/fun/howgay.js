a3fe85c3e2a96ad2b28c7bf33428741a204f6d2d
Black <82063930+Blackkk0@users.noreply.github.com>
2021-06-16 17:45:15 +0300

    Update howgay.js

--- commands/fun/howgay.js
+++ commands/fun/howgay.js
@@ -4,12 +4,11 @@ const { Color } = require("../../config.js");
 
 module.exports = {
   name: "howgay",
-  aliases: ["hg"],
+  aliases: ["gay"],
   description: "Show How Gay Member Is!",
   usage: "Howgay <Mention Member>",
   run: async (client, message, args) => {
-    //Start
-    message.delete();
+ 
     let Member =
       message.mentions.members.first() ||
       message.guild.members.cache.get(args[0]) ||
@@ -19,13 +18,13 @@ module.exports = {
 
     let embed = new MessageEmbed()
       .setColor(Color)
-      .setTitle(`Gay v2 Machine`)
+      .setTitle(`Gay Machine`)
       .setDescription(`${Member.user.username} Is ${Result}% Gay 🏳️‍🌈`)
       .setFooter(`Requested by ${message.author.username}`)
       .setTimestamp();
 
     message.channel.send(embed);
 
-    //End
+    
   }
 };
