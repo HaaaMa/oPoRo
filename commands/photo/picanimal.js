4386aec1def7498615af3d6ae19d2f0c2f50f12e
Black <82063930+Blackkk0@users.noreply.github.com>
2021-06-16 19:36:30 +0300

    Update picanimal.js

--- commands/photo/picanimal.js
+++ commands/photo/picanimal.js
@@ -9,8 +9,8 @@ module.exports = {
   usage: "Photo",
   run: async (client, message, args) => {
    
-    let replies = ["","","","","","","","","","","","",""];
-    
+    let replies = ["https://cdn.discordapp.com/attachments/608711490223996995/854056862944854036/a8afae559d1a02c5a96a5dd70717cf6d.jpg","https://cdn.discordapp.com/attachments/608711490223996995/854049204972421160/Screenshot_20210614-150832_Telegram.png","https://cdn.discordapp.com/attachments/608711490223996995/853975041870790656/c2253edf4d0de954a4703f6f074a2b10.jpg","https://cdn.discordapp.com/attachments/608711490223996995/853979788682592256/IMG_20190830_133014_403.jpg","https://cdn.discordapp.com/attachments/608711490223996995/853969776577740830/f1893b45ce7e234b517a46bf9875ac69.jpg","https://cdn.discordapp.com/attachments/608711490223996995/853812499181862922/image0.jpg","https://cdn.discordapp.com/attachments/608711490223996995/853043268602691594/images_3.jpeg"];
+
     let result = Math.floor((Math.random() * replies.length));
     
     let photoembed = new Discord.MessageEmbed()
