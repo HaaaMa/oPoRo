8d57855f28ab4cd3dc84c3a324d2ca3ffc63b7a7
HaaaMa <80664368+HaaaMa@users.noreply.github.com>
2021-06-26 13:09:17 +0300

    Update picgirl.js

--- commands/photo/picgirl.js
+++ commands/photo/picgirl.js
@@ -4,7 +4,7 @@ const { Prefix } = require("../../config.js");
 
 module.exports = {
   name: "picgirl",
-  aliases: [],
+  aliases: ["pgirl", "Pgirl"],
   description: "Show Photo",
   usage: "Photo",
   run: async (client, message, args) => {
@@ -17,7 +17,7 @@ module.exports = {
 
     .setTitle("Girl Photo")
     
-      .setColor("black")
+      .setColor("RANDOM")
     .setFooter(`${message.author.tag} `, message.author.avatarURL)
     .setImage(replies[result]);
 
