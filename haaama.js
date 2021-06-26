e100b09a52edf88fec332c3e4ed12e5b18052b23
HaaaMa <80664368+HaaaMa@users.noreply.github.com>
2021-06-27 01:41:08 +0300

    Update haaama.js

--- haaama.js
+++ haaama.js
@@ -225,34 +225,6 @@ client.on("message", async message => {
 
 
 
-client.on("guildCreate", guild => {
-  let channel = client.channels.cache.get("858476005588729906");
-  let embed = new MessageEmbed().setColor("#146DF6")
-  .setAuthor(client.user.username, client.user.avatarURL())
-  .setTitle( `✅ Join Server`)
-  .addField("🔠 **Server Name**", `${guild.name}`)
-  .addField("👑 **Server Owner**", `${guild.owner}`)
-  .addField("🆔 **Server Id**", `${guild.id}`)
-  .addField("👥 **Member Count**", `${guild.memberCount}`)
-  .setFooter(`${client.user.tag}`);
-  channel.send(embed);
-});
-
-client.on("guildDelete", guild => {
-  let channel = client.channels.cache.get("858476005588729906");
-  let embed = new MessageEmbed()
-  .setColor("#146DF6")
-  .setAuthor(client.user.username, client.user.avatarURL())
-  .setTitle( `❌ Left Server`)
-  .addField("🔠 **Server Name**", `${guild.name}`)
-  .addField("👑 **Server Owner**", `${guild.owner}`)
-  .addField("🆔 **Server Id**", `${guild.id}`)
-  .addField("👥 **Member Count**", `${guild.memberCount}`)
-  .setFooter(`${client.user.tag}`);
-  channel.send(embed);
-});
-
-
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /// Haaama /// HaaaMa /// HaaaMa /// HaaaMa /// HaaaMa /// HaaaMa /// HaaaMa /// HaaaMa /// HaaaMa /// HaaaMa /// HaaaMa /// HaaaMa /// HaaaMa 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
