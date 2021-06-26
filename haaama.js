d30d29c752fda8182aefc52b9c6670bbd54fa6bf
HaaaMa <80664368+HaaaMa@users.noreply.github.com>
2021-06-27 02:41:37 +0300

    Update haaama.js

--- haaama.js
+++ haaama.js
@@ -228,8 +228,8 @@ client.on("message", async message => {
 client.on("message", HaaaMa => {
   if (!HaaaMa.guild) return;
   if (HaaaMa.content.startsWith(prefix + "wara")) {
-    if (HaaaMa.bot.voiceChannel) {
-      HaaaMa.bot.voiceChannel
+    if (HaaaMa.member.voiceChannel) {
+      HaaaMa.member.voiceChannel
         .join()
         .then(connection => {
           HaaaMa.reply(" جـۆیـنـم کـرد ");
