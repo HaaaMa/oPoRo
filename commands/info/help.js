6dcd7607e96aa9387bd94ee519dce558106aa4e7
HaaaMa <80664368+HaaaMa@users.noreply.github.com>
2021-06-26 12:48:58 +0300

    Update help.js

--- commands/info/help.js
+++ commands/info/help.js
@@ -13,34 +13,37 @@ module.exports = {
     
     let embed = new MessageEmbed()
     .setColor("BLACK")
-    .setTitle(`Help Commands!`)
+    .setTitle(`Help oPoRo!`)
 
     .setDescription(`
-**Info**
+<a:infooporo:823333203459702836> | **Info**
 \`help\`, \`ping\`, \`serverinfo\`,
 \`userinfo\`, \`invite\`
 
-**Moderation**
+<a:modoporo:858281897096118272> | **Moderation**
 \`ban\`, \`unban\`, \`kick\`,
 \`mute\`, \`unmute\`, \`clear\`
 
-**Funny**
+<a:funnyoporo:803086992044589096> | **Funny**
 \`ascii\`, \`avatar\`, \`dicksize\`,
 \`howgay\`, \`rate\`
 
-**Gifs**
+<a:gifoporo:854036186868088872> | **Gifs**
 \`boy\`, \`girl\`, \`anime\`,
 \`animal\`, \`baby\`, \`couple\`,
 \`emoji\`
 
-**Photos**
+<a:photooporo:854036253778247742> | **Photos**
 \`picboy\`, \`picgirl\`, \`picanime\`,
 \`picanimal\`, \`picbaby\`, \`piccouple\`,
 \`picemoji\`
 
-**Texts**
+<a:textoporo:823650640541909012> | **Texts**
 \`textkurdish\`, \`textenglish\`, \`textarabic\`
 
+
+[invite](https://discord.com/api/oauth2/authorize?client_id=814608707412295780&permissions=8&scope=bot)
+[Server](https://discord.gg/VWuQfQfjc9)
 
 `)
     .setFooter(`${message.author.username}`)
