c189bcdc07b4a42b784900530c4d19ae75a77685
HaaaMa <80664368+HaaaMa@users.noreply.github.com>
2021-06-26 13:07:35 +0300

    Update picboy.js

--- commands/photo/picboy.js
+++ commands/photo/picboy.js
@@ -4,7 +4,7 @@ const { Prefix } = require("../../config.js");
 
 module.exports = {
   name: "picboy",
-  aliases: [],
+  aliases: ["pboy", "Pboy"],
   description: "Show Photo",
   usage: "Photo",
   run: async (client, message, args) => {
