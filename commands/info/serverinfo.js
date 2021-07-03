c30f336a138c5782fea04b0aab47490c400c4ea7
HaaaMa <80664368+HaaaMa@users.noreply.github.com>
2021-07-03 13:53:27 +0300

    Update serverinfo.js

--- commands/info/serverinfo.js
+++ commands/info/serverinfo.js
@@ -33,25 +33,7 @@ module.exports = {
       .setFooter(`Requested by ${message.author.username}`)
       .setTimestamp();
 
-client.on("message", prof => {
-  if (prof.content.startsWith(prefix + "server", "serverinfo", "Server", "infoserver")) {
-    var professor = new Discord.MessageEmbed()
-      .setAuthor(prof.guild.name)
-      .setThumbnail(prof.guild.iconURL())
-      .setTitle("**Info Server**")
-      .addField("", `${prof.guild.name}`)
-      .addField("", `${prof.guild.owner}`)
-      .addField("", `${prof.guild.id}`)
-      .addField("", `${prof.guild.createdAt.toLocaleString()}`)
-      .addField("", `${prof.guild.memberCount}`)
-      .addField("", `)
-     
-      .addField(" **Roles:**", ` ${prof.guild.roles.cache.size}`)
-      .setFooter(`Requested | ${prof.author.tag}`, prof.author.avatarURL())
-      .setTimestamp();
-    prof.channel.send(professor);
-  }
-});
+ 
 
     message.channel.send(embed);
 
