f0951da5df542bff1b621bb45bd07e5b9931bff9
HaaaMa <80664368+HaaaMa@users.noreply.github.com>
2021-07-03 14:58:58 +0300

    Create unlock.js

---
+++ commands/moderation/unlock.js
@@ -0,0 +1,37 @@
+const Discord = require("discord.js");
+const { MessageEmbed } = require("discord.js");
+const { Prefix } = require("../../config.js");
+
+module.exports = {
+  name: "unlock",
+  aliases: ["unlcok", "Unlock", "ul"],
+  description: "locked channel!",
+  usage: "unlocked",
+  run: async (client, message, args) => {
+
+
+ 
+   if (!message.member.hasPermission('MANAGE_SERVER', 'MANAGE_CHANNELS')) {
+   return message.channel.send("You don't have enough Permissions")
+   }
+   message.channel.overwritePermissions([
+     {
+        id: message.guild.id,
+        null : ['SEND_MESSAGES'],
+     },
+    ],);
+   const embed = new Discord.MessageEmbed()
+   .setTitle("")
+   .setTimestamp()
+   .setThumbnail(message.author.avatarURL({dynamic: "true"}))
+   .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
+   .setDescription(`
+🔓 Unlocked Channel
+Channel Name : <#${message.channel.id}>
+Locked By : <@${message.author.id}>
+Channel Status : Send Message
+`)
+   .setColor("BLACK");
+   await message.channel.send(embed);
+}
+}
