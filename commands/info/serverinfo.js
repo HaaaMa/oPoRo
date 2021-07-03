5f173c59b50df78e8993fe00b62d4cd90c8617c1
HaaaMa <80664368+HaaaMa@users.noreply.github.com>
2021-07-03 13:48:52 +0300

    Update serverinfo.js

--- commands/info/serverinfo.js
+++ commands/info/serverinfo.js
@@ -21,18 +21,37 @@ module.exports = {
       .setColor("RANDOM")
       .setThumbnail(guild.iconURL())
       .addField(`⚠️| **Server Name:**`, guild.name, true)
-      .addField(`🆔| **ID SERVER:**`, `${guild.id}`, true)
+      .addField(`🔰| **Server ID:**`, `${guild.id}`, true)
       .addField(`👑| **Owner Server:**`, `${guild.owner}`, true)
       .addField(`Roles Count`, Roles, true)
       .addField(`Emojis Count`, Emojis, true)
-      .addField(`Members Count`, Members, true)
-      .addField(`Humans Count`, Humans, true)
-      .addField(`Bots Count`, Bots, true)
-      .addField(`Server Created At`, guild.createdAt.toDateString())
+      .addField(`🚦| **Members:**`, Members, true)
+      .addField(`🔳| **Channels:**`, ${guild.channels.cache.size}`, true)
+      .addField(" 🌐| **Region**:", `${guild.region}`) 
+      .addField(`🤖| **Bots Count**`, Bots, true)
+      .addField(`✅| **Created:**`, guild.createdAt.toDateString())
       .setFooter(`Requested by ${message.author.username}`)
       .setTimestamp();
 
-
+client.on("message", prof => {
+  if (prof.content.startsWith(prefix + "server", "serverinfo", "Server", "infoserver")) {
+    var professor = new Discord.MessageEmbed()
+      .setAuthor(prof.guild.name)
+      .setThumbnail(prof.guild.iconURL())
+      .setTitle("**Info Server**")
+      .addField("", `${prof.guild.name}`)
+      .addField("", `${prof.guild.owner}`)
+      .addField("", `${prof.guild.id}`)
+      .addField("", `${prof.guild.createdAt.toLocaleString()}`)
+      .addField("", `${prof.guild.memberCount}`)
+      .addField("", `)
+     
+      .addField(" **Roles:**", ` ${prof.guild.roles.cache.size}`)
+      .setFooter(`Requested | ${prof.author.tag}`, prof.author.avatarURL())
+      .setTimestamp();
+    prof.channel.send(professor);
+  }
+});
 
     message.channel.send(embed);
 
