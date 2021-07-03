8bd2d14821a9c9b8a27bce51711e2ff33fdf505a
HaaaMa <80664368+HaaaMa@users.noreply.github.com>
2021-07-03 14:57:43 +0300

    Update lock.js

--- commands/moderation/lock.js
+++ commands/moderation/lock.js
@@ -3,8 +3,34 @@ const { MessageEmbed } = require("discord.js");
 const { Prefix } = require("../../config.js");
 
 module.exports = {
-  name: "slowmode",
-  aliases: ["sm"],
-  description: "Slow mode channel!",
-  usage: "slowmode <channel>",
+  name: "lock",
+  aliases: ["lock", "Lock", "lk"],
+  description: "locked channel!",
+  usage: "locked",
   run: async (client, message, args) => {
+
+
+   if (!message.member.hasPermission('MANAGE_SERVER', 'MANAGE_CHANNELS')) {
+   return message.channel.send("You don't have enough Permissions")
+   }
+   message.channel.overwritePermissions([
+     {
+        id: message.guild.id,
+        deny : ['SEND_MESSAGES'],
+     },
+    ],);
+   const embed = new Discord.MessageEmbed()
+   .setTitle("")
+   .setTimestamp()
+   .setThumbnail(message.author.avatarURL({dynamic: "true"}))
+   .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
+   .setDescription(`
+🔒 Locked Channel
+Channel Name : <#${message.channel.id}>
+Locked By : <@${message.author.id}>
+Channel Status : Send Message
+`)
+   .setColor("BLACK");
+   await message.channel.send(embed);
+}
+}
