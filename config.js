406c9ed7fc08998254d99d8e8da8a41e484b90fd
HaaaMa <80664368+HaaaMa@users.noreply.github.com>
2021-07-16 20:28:16 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-exports.Prefix = `1`;
-exports.Token = `ODE0NjA4NzA3NDEyMjk1Nzgw.YDgVeg.G6j6CiNgwFZYb2gPNQy5i9MaAX8`;
-//// token la Bashi haaama.js  la xwar xwar dabne agar na esh naka
+exports.Prefix = `HaaaMa / Prefix`;
+exports.Token = `HaaaMa / Token`;
+//// token la Bashi haaama.js dabne agar na esh naka
